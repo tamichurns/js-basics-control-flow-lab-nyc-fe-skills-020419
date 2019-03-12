982b3d9951dd4739958cfd5116c30ade277db97e
Tami Churns <tchurns@paperlesspost.com>
2019-03-12 00:06:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function scuberGreetingForFeet(distance) {
     return('This one is on me!')
   } else if (distance >= 2000) {
     return('I will gladly take your thirty bucks.')
-  } else if (distance )
+  } else if (distance >= 2500) {
+    return('No can do')
+  }
 }
 
